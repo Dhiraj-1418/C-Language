7bb0a2fd46fb57b5a178cbc9eab9c3de3366aafa
Dhiraj-1418 <8475dhirajyadav@gmail.com>
2024-08-05 11:12:58 +0530

    Add files via upload

---
+++ all loop/1st.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+		int i= 1;
+	printf("Program to print 1 to 10 using a do-while loop.\n\n");
+	
+
+	
+	
+	do
+	{
+		printf("%d ",i);
+		i++;
+	}while(i<=10);
+	
+	
+}
---
+++ all loop/4TH-.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int i=1,n,first=0,second=1,ans;
+	
+	printf("Enter n:");
+	scanf("%d",&n);
+	printf("0 1 ");
+	for(i=1; i<=n; i++)
+	{
+		ans = first + second;
+		first=second;
+		second=ans;
+		printf("%d ",ans);
+	}
+	
+	
+}
